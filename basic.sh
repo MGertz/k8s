83e9b99c1e1451ae21a4e7c67c92b3e3b2acce48
Michael Gertz <michael@ringhus.dk>
2022-09-16 13:14:12 +0200

    Update basic.sh

--- basic.sh
+++ basic.sh
@@ -2,47 +2,33 @@
 number_of_actions=16
 
 # variables
-echo .
-echo .
-echo "1/$number_of_actions Collect variables"
+echo -e "\n\n1/$number_of_actions Collect variables"
 dist_version=$(lsb_release -cs)
 lsb_dist="$(. /etc/os-release && echo "$ID")"
 
 # Install nessesary packages
-echo .
-echo .
-echo "2/$number_of_actions Update and Install needed packages."
+echo -e "\n\n2/$number_of_actions Update and Install needed packages."
 sudo apt-get update > /dev/null
 sudo apt-get upgrade -y > /dev/null
 sudo apt-get autoremove -y > /dev/null
 sudo apt-get install apt-transport-https ca-certificates curl gnupg2 vim git software-properties-common -y > /dev/null
 
 # Disable swap
-echo .
-echo .
-echo "3/$number_of_actions Swap off"
+echo -e "\n\n3/$number_of_actions Swap off"
 sudo swapoff -a > /dev/null
 
 # Remove Swap from fstab, else it exists after reboot
-echo .
-echo .
-echo "4/$number_of_actions Disable swap"
+echo -e "\n\n4/$number_of_actions Disable swap"
 sudo sed -i "s/\/swap.img/#\/swap.img/g" /etc/fstab
 
 # Disable firewall
-echo .
-echo .
-echo "5/$number_of_actions Stopping firewall"
+echo -e "\n\n5/$number_of_actions Stopping firewall"
 sudo systemctl stop ufw
-echo .
-echo .
-echo "6/$number_of_actions Disabling firewall"
+echo -e "\n\n6/$number_of_actions Disabling firewall"
 sudo systemctl disable ufw
 
 # add kernel modules
-echo .
-echo .
-echo "7/$number_of_actions Adding kerkel module"
+echo -e "\n\n7/$number_of_actions Adding kerkel module"
 sudo modprobe overlay
 sudo modprobe br_netfilter
 
@@ -57,41 +43,29 @@ net.bridge.bridge-nf-call-iptables = 1
 EOF
 
 # Reload sysctl
-echo .
-echo .
-echo "8/$number_of_actions Reload Kernel modules"
+echo -e "\n\n8/$number_of_actions Reload Kernel modules"
 sudo sysctl --system
 
 # Remove all old versions of docker
-echo .
-echo .
-echo "9/$number_of_actions Removed oldversions of docker"
+echo -e "\n\n9/$number_of_actions Removed oldversions of docker"
 sudo apt remove docker-compose docker.io-doc docker2aci docker-doc docker-ce docker.io docker docker-clean docker-registry docker-ce -y
 
 # Adding docker repository
-echo .
-echo .
-echo "10/$number_of_actions Adding docker repository"
+echo -e "\n\n10/$number_of_actions Adding docker repository"
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor --yes -o /etc/apt/keyrings/docker.gpg
 sudo echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/$lsb_dist $dist_version stable" > /etc/apt/sources.list.d/docker.list
 sudo apt update
 
 # Install Docker
-echo .
-echo .
-echo "11/$number_of_actions Install new docker version"
+echo -e "\n\n11/$number_of_actions Install new docker version"
 sudo apt install -y containerd.io docker-ce docker-ce-cli
 
 # Make required directory
-echo .
-echo .
-echo "12/$number_of_actions creating directories"
+echo -e "\n\n12/$number_of_actions creating directories"
 sudo mkdir -p /etc/systemd/system/docker.service.d
 
 # Create daemon json config file
-echo .
-echo .
-echo "13/$number_of_actions Create daemon config file"
+echo -e "\n\n13/$number_of_actions Create daemon config file"
 sudo tee /etc/docker/daemon.json <<EOF
 {
   "exec-opts": ["native.cgroupdriver=systemd"],
@@ -104,17 +78,11 @@ sudo tee /etc/docker/daemon.json <<EOF
 EOF
 
 # Start and enable Services
-echo .
-echo .
-echo "14/$number_of_actions Restarting daemon"
+echo -e "\n\n14/$number_of_actions Restarting daemon"
 sudo systemctl daemon-reload 
-echo .
-echo .
-echo "15/$number_of_actions Restarting docker"
+echo -e "\n\n15/$number_of_actions Restarting docker"
 sudo systemctl restart docker
-echo .
-echo .
-echo "16/$number_of_actions Enable daemon"
+echo -e "\n\n16/$number_of_actions Enable daemon"
 sudo systemctl enable docker
 
 
