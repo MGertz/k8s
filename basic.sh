51f0e6031351b40cbe811e4915c577694ec9b0c4
Michael Gertz <michael@ringhus.dk>
2022-09-16 14:00:59 +0200

    Update basic.sh

--- basic.sh
+++ basic.sh
@@ -39,12 +39,12 @@ sudo modprobe br_netfilter
 cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
 overlay
 br_netfilter
-EOF > /dev/null
+EOF
 
 cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
 net.bridge.bridge-nf-call-ip6tables = 1
 net.bridge.bridge-nf-call-iptables = 1
-EOF > /dev/null
+EOF
 
 # Reload sysctl
 echo " 8/$number_of_actions Reload Kernel modules"
