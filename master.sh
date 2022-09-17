a748d8b4a3ca6a45791257bf5ed9e6b96b0a65db
Michael Gertz <michael@ringhus.dk>
2022-09-17 12:09:10 +0200

    enable kubelet

--- master.sh
+++ master.sh
@@ -3,3 +3,9 @@
 # Kør basic scriptet.
 curl -fsSL https://raw.githubusercontent.com/MGertz/k8s/main/basic.sh | sudo sh
 
+# Enable kubelet
+echo "21/$number_of_actions Enable kubelet"
+sudo systemctl enable kubelet
+
+
+sudo kubeadm config images pull
