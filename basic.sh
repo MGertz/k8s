3691a841c7d7c72456632dd493f38f6f30d2fa8a
Michael Gertz <michael@ringhus.dk>
2022-09-21 22:26:29 +0200

    Update basic.sh

--- basic.sh
+++ basic.sh
@@ -1,128 +1,224 @@
 #!/bin/bash
 clear
+RED='\033[0;31m'
+BLUE='\033[0;34m'
+CYAN='\033[0;36m'
+NC='\033[0m'
 
-echo '  ^ ^                      '
-echo ' (O,O)                     '
-echo ' (   ) Kubernetes Setup    '
-echo ' -"-"----------------------\n\n'
+START_COLOR=${CYAN}
+
+#echo -n ${LINES}
+#echo -n ${COLUMNS}
+
+echo -e ${CYAN}
+echo ' _  ___    _ ____  ______ _____  _   _ ______ _______ ______  _____  '
+echo '| |/ / |  | |  _ \|  ____|  __ \| \ | |  ____|__   __|  ____|/ ____| '
+echo "| ' /| |  | | |_) | |__  | |__) |  \| | |__     | |  | |__  | (___   "
+echo '|  < | |  | |  _ <|  __| |  _  /| . ` |  __|    | |  |  __|  \___ \  '
+echo '| . \| |__| | |_) | |____| | \ \| |\  | |____   | |  | |____ ____) | '
+echo '|_|\_\\____/|____/|______|_|  \_\_| \_|______|  |_|  |______|_____/  '
+echo -e "\n\n"
+echo -e ${RED}
 
 
-number_of_actions=20
+
+number_of_actions=28
 
 # variables
-echo " 1/$number_of_actions Collect variables"
+
 dist_version=$(lsb_release -cs)
 lsb_dist="$(. /etc/os-release && echo "$ID")"
 
-# Install nessesary packages
-echo " 2/$number_of_actions Update and Install needed packages."
-echo "   - apt update"
-sudo apt-get update > /dev/null
-echo "   - apt upgrade"
-#sudo apt-get upgrade -y > /dev/null
-sudo DEBIAN_FRONTEND=nointeractive apt-get dist-upgrade -y > /dev/null
-echo "   - apt autoremove"
-sudo apt-get autoremove -y > /dev/null
-echo "   - apt install"
-sudo apt-get install apt-transport-https ca-certificates curl gnupg2 vim git software-properties-common -y > /dev/null
-
-# Disable swap
-echo " 3/$number_of_actions Swap off"
-sudo swapoff -a > /dev/null
-
-# Remove Swap from fstab, else it exists after reboot
-echo " 4/$number_of_actions Disable swap"
-sudo sed -i "s/\/swap.img/#\/swap.img/g" /etc/fstab
-
-# Disable firewall
-echo " 5/$number_of_actions Stopping firewall"
-sudo systemctl stop ufw > /dev/null
-echo " 6/$number_of_actions Disabling firewall"
-sudo systemctl disable ufw > /dev/null
-
-# add kernel modules
-echo " 7/$number_of_actions Adding kerkel module"
-sudo modprobe overlay
-sudo modprobe br_netfilter
-
-sudo touch /etc/modules-load.d/k8s.conf
-sudo echo "overlay" >> /etc/modules-load.d/k8s.conf
-sudo echo "br_netfilter" >> /etc/modules-load.d/k8s.conf
-
-sudo touch /etc/sysctl.d/k8s.conf
-sudo echo "net.bridge.bridge-nf-call-ip6tables = 1" >> /etc/sysctl.d/k8s.conf
-sudo echo "net.bridge.bridge-nf-call-iptables = 1" >> /etc/sysctl.d/k8s.conf
-sudo echo "net.ipv4.ip_forward = 1" >> /etc/sysctl.d/k8s.conf
-
-# Reload sysctl
-echo " 8/$number_of_actions Reload Kernel modules"
-sudo sysctl --system > /dev/null
-
-# Remove all old versions of docker
-echo " 9/$number_of_actions Removed oldversions of docker"
-sudo apt-get remove docker-compose docker.io-doc docker2aci docker-doc docker-ce docker.io docker docker-clean docker-registry docker-ce -y > /dev/null
-
-# Adding docker repository
-echo "10/$number_of_actions Adding docker repository"
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor --yes -o /etc/apt/keyrings/docker.gpg
-sudo echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/$lsb_dist $dist_version stable" > /etc/apt/sources.list.d/docker.list
-sudo apt-get update > /dev/null
-
-# Install Docker
-echo "11/$number_of_actions Install new docker version"
-sudo apt-get install -y containerd.io docker-ce docker-ce-cli > /dev/null
-
-# Make required directory
-echo "12/$number_of_actions creating directories"
-sudo mkdir -p /etc/systemd/system/docker.service.d
-
-# Create daemon json config file
-echo "13/$number_of_actions Create daemon config file"
-sudo touch /etc/docker/daemon.json
-sudo echo "{" >> /etc/docker/daemon.json
-sudo echo '  "exec-opts": ["native.cgroupdriver=systemd"],' >> /etc/docker/daemon.json
-sudo echo '  "log-driver": "json-file",' >> /etc/docker/daemon.json
-sudo echo '  "log-opts": {' >> /etc/docker/daemon.json
-sudo echo '    "max-size": "100m"' >> /etc/docker/daemon.json
-sudo echo '    "max-size": "100m"' >> /etc/docker/daemon.json
-sudo echo '  },' >> /etc/docker/daemon.json
-sudo echo '  "storage-driver": "overlay2"' > /etc/docker/daemon.json
-sudo echo '}' >> /etc/docker/daemon.json
-
-# Start and enable Services
-echo "14/$number_of_actions Restarting daemon"
-sudo systemctl daemon-reload  > /dev/null
-echo "15/$number_of_actions Restarting docker"
-sudo systemctl restart docker > /dev/null
-echo "16/$number_of_actions Enable daemon"
-sudo systemctl enable docker > /dev/null
-
-# Adding repository for kubernetes
-echo "17/$number_of_actions Adding repository for kubernetes"
-echo "   - Google cloud package"
-curl -fsSL  https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/k8s.gpg
 
 
-echo "   - deb package"
+# 1
+function remove_old_docker() {
+    # Remove all old versions of docker
+    echo -e "${START_COLOR} 1/$number_of_actions Removed oldversions of docker${NC}"
+    sudo apt-get remove docker-compose docker.io-doc docker2aci docker-doc docker-ce docker.io docker docker-clean docker-registry docker-ce -y
+}
+
+# 2 
+function prep_system() {
+    #disable automaticly upgrade
+    echo -e "${START_COLOR} 2/$number_of_actions disable automaticly upgrade${NC}"
+    echo -e "APT::Periodic::Update-Package-Lists \"0\";\nAPT::Periodic::Unattended-Upgrade \"0\";\n"  | sudo tee /etc/apt/apt.conf.d/20auto-upgrades
+}
+
+# 
+function add_missing_repositories() {
+    # Adding missing repository
+    echo -e "${START_COLOR} 3/$number_of_actions Adding docker repository${NC}"
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor --yes -o /etc/apt/keyrings/docker.gpg
+    sudo echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/$lsb_dist $dist_version stable" > /etc/apt/sources.list.d/docker.list
+
+    echo -e "${START_COLOR} 4/$number_of_actions Adding repository for kubernetes${NC}"
+
+    echo -e "${START_COLOR}   - Google cloud package${NC}"
+    curl -fsSL  https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/k8s.gpg
+
+    echo -e "${START_COLOR}   - deb package${NC}"
+    sudo echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+    # Install kubernetes
+    echo -e "${START_COLOR} 5/$number_of_actions apt update${NC}"
+    sudo apt-get update
+}
+
+# 4
+function install_packages() {
+    # Install nessesary packages
+    echo -e "${START_COLOR} 6/$number_of_actions Update and Install needed packages.${NC}"
+
+    echo -e "${START_COLOR}   - apt update${NC}"
+    sudo apt-get update
+
+    echo -e "${START_COLOR}   - apt upgrade${NC}"
+    sudo DEBIAN_FRONTEND=nointeractive apt-get dist-upgrade -y
+
+#    echo -e "${START_COLOR}   - apt autoremove${NC}"
+#    sudo apt-get autoremove -y
+
+    echo -e "${START_COLOR}   - apt install${NC}"
+    #sudo apt-get install -y apt-transport-https ca-certificates curl gnupg2 vim git software-properties-common containerd.io docker-ce docker-ce-cli kubelet kubeadm kubectl
+    sudo apt-get install -y apt-transport-https ca-certificates curl gnupg2 vim git software-properties-common docker-ce docker-ce-cli kubelet kubeadm kubectl
+
+    echo -e "${START_COLOR}   - hold kubexxx${NC}"
+    sudo apt-mark hold kubelet kubeadm kubectl
+ 
+}
+
+# 5
+function disable_swap() {
+    # Disable swap
+    echo -e "${START_COLOR} 7/$number_of_actions Swap off${NC}"
+    sudo swapoff -a
+
+    # Remove Swap from fstab, else it exists after reboot
+    echo -e "${START_COLOR} 8/$number_of_actions Disable swap${NC}"
+    sudo sed -i "s/\/swap.img/#\/swap.img/g" /etc/fstab
+}
+
+# 6
+function disable_firewall() {
+    # Disable firewall
+    echo -e "${START_COLOR} 9/$number_of_actions Stopping firewall${NC}"
+    sudo systemctl stop ufw
+    echo -e "${START_COLOR}10/$number_of_actions Disabling firewall${NC}"
+    sudo systemctl disable ufw
+}
+
+#7 
+function add_kernel_modules() {
+    # add kernel modules
+    echo -e "${START_COLOR}11/$number_of_actions Adding kerkel module${NC}"
+    sudo modprobe overlay
+    sudo modprobe br_netfilter
+
+    sudo touch /etc/modules-load.d/k8s.conf
+    sudo echo "overlay" >> /etc/modules-load.d/k8s.conf
+    sudo echo "br_netfilter" >> /etc/modules-load.d/k8s.conf
+
+    sudo touch /etc/sysctl.d/k8s.conf
+    sudo echo "net.bridge.bridge-nf-call-ip6tables = 1" >> /etc/sysctl.d/k8s.conf
+    sudo echo "net.bridge.bridge-nf-call-iptables = 1" >> /etc/sysctl.d/k8s.conf
+    sudo echo "net.ipv4.ip_forward = 1" >> /etc/sysctl.d/k8s.conf
+
+    # Reload sysctl
+    echo -e "${START_COLOR}12/$number_of_actions Reload Kernel modules${NC}"
+    sudo sysctl --system
+}
+
+# 8
+function config_docker() {
+    # Make required directory
+    echo -e "${START_COLOR}13/$number_of_actions creating directories${NC}"
+    sudo mkdir -p /etc/systemd/system/docker.service.d
+
+    # Create daemon json config file
+    echo -e "${START_COLOR}14/$number_of_actions Create daemon config file${NC}"
+    sudo tee /etc/docker/daemon.json <<EOF
+{
+    "exec-opts": ["native.cgroupdriver=systemd"],
+    "log-driver": "json-file",
+    "log-opts": {
+      "max-size": "100m"
+    },
+    "storage-driver": "overlay2"
+}
+EOF
 
 
-# echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+    # Start and enable Services
+    echo -e "${START_COLOR}15/$number_of_actions Restarting daemon${NC}"
+    sudo systemctl daemon-reload 
+    
+    echo -e "${START_COLOR}16/$number_of_actions Restarting docker${NC}"
+    sudo systemctl restart docker
+    
+    echo -e "${START_COLOR}17/$number_of_actions Enable daemon${NC}"
+    sudo systemctl enable docker
+}
 
-sudo touch /etc/apt/sources.list.d/kubernetes.list
-sudo echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" > /etc/apt/sources.list.d/kubernetes.list
+# 9
+function install_cri_docker() {
+    # get the latest release version:
+    echo -e "${START_COLOR}18/$number_of_actions get latest version of cri-dockerd${NC}"
+    VER=$(curl -s https://api.github.com/repos/Mirantis/cri-dockerd/releases/latest|grep tag_name | cut -d '"' -f 4|sed 's/v//g')
+    echo $VER
 
 
-sudo echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/$lsb_dist $dist_version stable" > /etc/apt/sources.list.d/docker.list
+    #We can then download the archive file from Github cri-dockerd releases page.
+    echo -e "${START_COLOR}19/$number_of_actions Download latest version of cri-dockerd${NC}"
+    wget https://github.com/Mirantis/cri-dockerd/releases/download/v${VER}/cri-dockerd-${VER}.amd64.tgz 2>&1
+    
+    echo -e "${START_COLOR}20/$number_of_actions extract latest version of cri-dockerd${NC}"
+    tar xvf cri-dockerd-${VER}.amd64.tgz
 
-# Install kubernetes
-echo "18/$number_of_actions apt update"
-sudo apt-get update > /dev/null
-echo "19/$number_of_actions apt install packages"
-sudo apt-get install kubelet kubeadm kubectl -y > /dev/null
-echo "20/$number_of_actions lock kubernetes to specific version"
-sudo apt-mark hold kubelet kubeadm kubectl
+    #Move cri-dockerd binary package to /usr/local/bin directory
+    echo -e "${START_COLOR}21/$number_of_actions move latest version of cri-dockerd${NC}"
+    sudo mv cri-dockerd/cri-dockerd /usr/local/bin/
 
 
+    # Configure systemd units for cri-dockerd:
+    echo -e "${START_COLOR}22/$number_of_actions download conf file for service${NC}"
+    wget https://raw.githubusercontent.com/Mirantis/cri-dockerd/master/packaging/systemd/cri-docker.service 2>&1
+    
+    echo -e "${START_COLOR}23/$number_of_actions download conf file for socket${NC}"
+    wget https://raw.githubusercontent.com/Mirantis/cri-dockerd/master/packaging/systemd/cri-docker.socket 2>&1
+    
+    echo -e "${START_COLOR}24/$number_of_actions move files to correct location${NC}"
+    sudo mv cri-docker.socket cri-docker.service /etc/systemd/system/
+    
+    echo -e "${START_COLOR}25/$number_of_actions updated conf file${NC}"
+    sudo sed -i -e 's,/usr/bin/cri-dockerd,/usr/local/bin/cri-dockerd,' /etc/systemd/system/cri-docker.service
+
+    # Start and enable the services
+    echo -e "${START_COLOR}26/$number_of_actions reload daemon${NC}"
+    sudo systemctl daemon-reload
+    
+    echo -e "${START_COLOR}27/$number_of_actions enable cri-docker.service${NC}"
+    sudo systemctl enable cri-docker.service
+    
+    echo -e "${START_COLOR}28/$number_of_actions enable cri-docker.socket${NC}"
+    sudo systemctl enable --now cri-docker.socket
+}
+
+# Function to collect all needed function
+function run() {
+    remove_old_docker
+    prep_system
+    add_missing_repositories
+    install_packages
+    disable_swap
+    disable_firewall
+    add_kernel_modules
+    config_docker
+    install_cri_docker
+}
+
+# Execute the functions
+run
 
 
 
